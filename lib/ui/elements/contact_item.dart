bbe9006f82f8e5ffa29c9d16d4bd1d4a0fa9a88b
bo bo <172291671+bo-bo0@users.noreply.github.com>
2026-09-04 18:36:27 +0200

    add contact form ui

---
+++ lib/ui/elements/contact_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ContactItem extends StatelessWidget {
+  const ContactItem({super.key, required this.name, required this.number});
+
+  final String name;
+  final String number;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(name),
+      subtitle: Text(number),
+    );
+  }
+}
