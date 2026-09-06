442901b6f343314c0cf18ceeee0365420ceba0c9
bo bo <pierlucaperla@gmail.com>
2026-09-06 20:10:17 +0200

    add safe area to bottom input bar

--- lib/ui/elements/input/bottom_input_bar.dart
+++ lib/ui/elements/input/bottom_input_bar.dart
@@ -8,30 +8,33 @@ class BottomInputBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Row(
-        children: [
-          Expanded(
-              child: TextField(
-                controller: _inputController,
-                decoration: InputDecoration(
-                    hintText: "Scrivi qui...",
-                    border: OutlineInputBorder()
-                ),
-              )
-          ),
-          IconButton(
-              onPressed: () {
-                if (_inputController.text.isEmpty) {
-                  return;
-                }
-                ChatHelper.sendMessageInCurrentlyOpenedChat(_inputController.text);
-                _inputController.clear();
-              },
-              icon: Icon(Icons.send)
-          )
-        ],
+    return SafeArea(
+      top: false,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(
+                child: TextField(
+                  controller: _inputController,
+                  decoration: InputDecoration(
+                      hintText: "Scrivi qui...",
+                      border: OutlineInputBorder()
+                  ),
+                )
+            ),
+            IconButton(
+                onPressed: () {
+                  if (_inputController.text.isEmpty) {
+                    return;
+                  }
+                  ChatHelper.sendMessageInCurrentlyOpenedChat(_inputController.text);
+                  _inputController.clear();
+                },
+                icon: Icon(Icons.send)
+            )
+          ],
+        ),
       ),
     );
   }
