d64e36610b95f190ee30a86d3b3bd3773ccbcab2
bo bo <172291671+bo-bo0@users.noreply.github.com>
2026-09-10 22:00:20 +0200

    small dart fixes

--- lib/ui/elements/contact_list.dart
+++ lib/ui/elements/contact_list.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wostup/ui/elements/contact_item.dart';
 import 'package:wostup/utils/contacts/contacts_info_manager.dart';
